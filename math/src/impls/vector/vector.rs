10ce96d90c5920975e3cae650240724cde566d5b
Ken <lax.potcx1562@gmail.com>
2026-07-14 02:37:33 +0900

    impl/vector.rs subテストOK

--- math/src/impls/vector/vector.rs
+++ math/src/impls/vector/vector.rs
@@ -1,4 +1,4 @@
-use std::ops::Add;
+use std::ops::{Add, Sub};
 use crate::vector::Vector;
 
 impl<T, const N: usize> Add for Vector<T, N>
@@ -10,7 +10,23 @@ where
         let mut values = self.values;
 
         for i in 0..N {
-            values[i] = values[i] + rhs.values[i];
+            values[i] = self.values[i] + rhs.values[i];
+        }
+
+        Self { values }
+    }
+}
+
+impl<T, const N: usize> Sub for Vector<T, N>
+where
+    T: Sub<Output = T> + Copy,
+{
+    type Output = Self;
+    fn sub(self, rhs: Self) -> Self {
+        let mut values = self.values;
+
+        for i in 0..N {
+            values[i] = self.values[i] - rhs.values[i];
         }
 
         Self { values }
@@ -34,6 +50,18 @@ mod tests {
     }
 
     #[test]
+    fn vector_sub_i32() {
+        let a = Vector::new([7, 2, 5]);
+        let b = Vector::new([4, 5, 6]);
+
+        let c = a - b;
+
+        assert_eq!(c[0], 3);
+        assert_eq!(c[1], -3);
+        assert_eq!(c[2], -1);
+    }
+
+    #[test]
     fn vector_add_f32() {
         let a = Vector::new([1.5_f32, 2.5, 3.5]);
         let b = Vector::new([0.5_f32, 1.5, 2.5]);
@@ -46,11 +74,31 @@ mod tests {
     }
 
     #[test]
+    fn vector_sub_f32() {
+        let a = Vector::new([1.5_f32, -2.5, 3.5]);
+        let b = Vector::new([0.5_f32, 1.5, -2.5]);
+
+        let c = a - b;
+
+        assert_eq!(c[0], 1.0);
+        assert_eq!(c[1], -4.0);
+        assert_eq!(c[2], 6.0);
+    }
+
+    #[test]
     fn vector_add_zero() {
         let a = Vector::new([3, 6, 9]);
         let z = Vector::new([0, 0, 0]);
 
         assert_eq!(a + z, Vector::new([3, 6, 9]));
+    }
+
+    #[test]
+    fn vector_sub_zero() {
+        let a = Vector::new([3, 6, 9]);
+        let z = Vector::new([0, 0, 0]);
+
+        assert_eq!(a - z, Vector::new([3, 6, 9]));
     }
 
     #[test]
@@ -62,10 +110,26 @@ mod tests {
     }
 
     #[test]
+    fn vector_sub_single_element() {
+        let a = Vector::new([10]);
+        let b = Vector::new([20]);
+
+        assert_eq!(a - b, Vector::new([-10]));
+    }
+
+    #[test]
     fn vector_add_negative() {
         let a = Vector::new([10, -2, 5]);
         let b = Vector::new([-3, 8, -10]);
 
         assert_eq!(a + b, Vector::new([7, 6, -5]));
+    }
+
+    #[test]
+    fn vector_sub_negative() {
+        let a = Vector::new([10, -2, 5]);
+        let b = Vector::new([-3, 8, -10]);
+
+        assert_eq!(a - b, Vector::new([13, -10, 15]));
     }
 }
