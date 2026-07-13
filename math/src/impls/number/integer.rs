8e5fbe17f5e5162aa8136c5dbf1c1102c49d69bb
Ken <lax.potcx1562@gmail.com>
2026-07-13 23:53:49 +0900

    Integerまで型テスト完了

--- math/src/impls/number/integer.rs
+++ math/src/impls/number/integer.rs
@@ -7,3 +7,20 @@ macro_rules! impl_integer {
 }
 
 impl_integer!(i8, i16, i32, i64, i128, isize);
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn assert_integer<T: Integer>() {}
+
+    #[test]
+    fn signed_integer_types_implement_integer() {
+        assert_integer::<i8>();
+        assert_integer::<i16>();
+        assert_integer::<i32>();
+        assert_integer::<i64>();
+        assert_integer::<i128>();
+        assert_integer::<isize>();
+    }
+}
