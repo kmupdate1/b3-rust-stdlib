c78d4050dae644241b61fb9361967e791a7edaaf
Ken <lax.potcx1562@gmail.com>
2026-07-14 02:56:39 +0900

    impl/vector.rs mulテストOK

--- math/src/impls/vector/vector.rs
+++ math/src/impls/vector/vector.rs
@@ -1,4 +1,4 @@
-use std::ops::{Add, Sub};
+use std::ops::{Add, Mul, Sub};
 use crate::vector::Vector;
 
 impl<T, const N: usize> Add for Vector<T, N>
@@ -27,6 +27,22 @@ where
 
         for i in 0..N {
             values[i] = self.values[i] - rhs.values[i];
+        }
+
+        Self { values }
+    }
+}
+
+impl<T, const N: usize> Mul<T> for Vector<T, N>
+where
+    T: Mul<Output = T> + Copy,
+{
+    type Output = Self;
+    fn mul(self, rhs: T) -> Self {
+        let mut values = self.values;
+
+        for i in 0..N {
+            values[i] = self.values[i] * rhs;
         }
 
         Self { values }
@@ -62,6 +78,17 @@ mod tests {
     }
 
     #[test]
+    fn vector_mul_i32() {
+        let a = Vector::new([1, 2, 3]);
+
+        let ka = a * 3;
+
+        assert_eq!(ka[0], 3);
+        assert_eq!(ka[1], 6);
+        assert_eq!(ka[2], 9);
+    }
+
+    #[test]
     fn vector_add_f32() {
         let a = Vector::new([1.5_f32, 2.5, 3.5]);
         let b = Vector::new([0.5_f32, 1.5, 2.5]);
@@ -86,6 +113,17 @@ mod tests {
     }
 
     #[test]
+    fn vector_mul_f32() {
+        let a = Vector::new([1.5_f32, 2.5, 3.5]);
+
+        let ka = a * 3.4;
+
+        assert_eq!(ka[0], 1.5f32 * 3.4);
+        assert_eq!(ka[1], 2.5 * 3.4);
+        assert_eq!(ka[2], 3.5 * 3.4);
+    }
+
+    #[test]
     fn vector_add_zero() {
         let a = Vector::new([3, 6, 9]);
         let z = Vector::new([0, 0, 0]);
@@ -99,6 +137,13 @@ mod tests {
         let z = Vector::new([0, 0, 0]);
 
         assert_eq!(a - z, Vector::new([3, 6, 9]));
+    }
+
+    #[test]
+    fn vector_mul_zero() {
+        let a = Vector::new([3, 6, 9]);
+
+        assert_eq!(a * 0, Vector::new([0, 0, 0]));
     }
 
     #[test]
@@ -118,6 +163,13 @@ mod tests {
     }
 
     #[test]
+    fn vector_mul_single_element() {
+        let a = Vector::new([10]);
+
+        assert_eq!(a * 2, Vector::new([20]));
+    }
+
+    #[test]
     fn vector_add_negative() {
         let a = Vector::new([10, -2, 5]);
         let b = Vector::new([-3, 8, -10]);
@@ -131,5 +183,12 @@ mod tests {
         let b = Vector::new([-3, 8, -10]);
 
         assert_eq!(a - b, Vector::new([13, -10, 15]));
+    }
+
+    #[test]
+    fn vector_mul_negative() {
+        let a = Vector::new([10, -2, 5]);
+
+        assert_eq!(a * -2, Vector::new([-20, 4, -10]));
     }
 }
