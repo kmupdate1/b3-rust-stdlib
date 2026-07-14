a6a17682b1e5f7e79f27e069de198fa301fbee5f
Ken <lax.potcx1562@gmail.com>
2026-07-15 00:13:27 +0900

    quantity: math: rational/ratio基本部分定義

--- math/src/ratio/proportion.rs
+++ math/src/ratio/proportion.rs
@@ -1,6 +1,8 @@
-/// 全体に対する割合を表す。
-///
-/// Ratio のうち 0 <= value <= 1 を満たすもの。
+/**
+ * 全体に対する割合を表す。
+ *
+ * Ratio のうち 0 <= value <= 1 を満たすもの。
+ */
 
 pub struct Proportion<T> {
     value: T,
--- math/src/rational/fraction.rs
+++ math/src/rational/fraction.rs
@@ -1,4 +1,6 @@
-/// 分子・分母による有理数の表現。
+/**
+ * 分子・分母による有理数の表現。
+ */
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub struct Fraction<T> {
