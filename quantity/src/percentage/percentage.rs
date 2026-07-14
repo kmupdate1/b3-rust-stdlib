f22e1e61dc8bf7688277fef07646d21b10507932
Ken <lax.potcx1562@gmail.com>
2026-07-15 01:36:57 +0900

    quantity: percentage.rs骨格大体こんな感じ

--- quantity/src/percentage/percentage.rs
+++ quantity/src/percentage/percentage.rs
@@ -11,8 +11,9 @@ pub struct Percentage<T> {
 }
 
 impl<T> Percentage<T> {
-    pub fn new(value: T) -> Self {
-        Self { ratio: Ratio::new(value) }
+    #[deprecated(since = "0.2.0", note = "use `try_new` instead")]
+    pub fn new(ratio: Ratio<T>) -> Self {
+        Self { ratio }
     }
 
     pub fn ratio(&self) -> &Ratio<T> { &self.ratio }
@@ -24,12 +25,12 @@ impl<T> Percentage<T>
 where
     T: PartialEq,
 {
-    pub fn is_valid(&self) -> bool { todo!() }
+    pub fn is_valid(&self) -> bool { todo!("validation logic") }
 
     pub fn try_new(&self, ratio: Ratio<T>) -> Result<Self, PercentageError> {
-        todo!()
+        todo!("validation")
     }
 
-    pub fn from_percentage(value: T) -> Self { todo!() }
-    pub fn from_rational(value: T) -> Self { todo!() }
+    pub fn from_percentage(value: T) -> Self { todo!("Ratio::new(value)") }
+    pub fn from_rational(value: T) -> Self { todo!("Ratio::new(value)") }
 }
