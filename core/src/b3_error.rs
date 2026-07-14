7640464a6ae48f6471e9f628188c1e224aa81af0
Ken <lax.potcx1562@gmail.com>
2026-07-14 17:14:25 +0900

    b3-core開発のための準備

--- core/src/b3_error.rs
+++
@@ -1,6 +0,0 @@
-/// 論理破綻時のエラー通知
-#[derive(Debug, PartialEq, Eq, Clone, Copy)]
-pub enum B3Error {
-    Invalid,
-    Internal,
-}
--- core/src/b3_invariant.rs
+++
@@ -1,4 +0,0 @@
-/// すべてのB3型が満たすべき契約
-pub trait B3Invariant<T> {
-    fn validate(value: &T) -> bool;
-}
