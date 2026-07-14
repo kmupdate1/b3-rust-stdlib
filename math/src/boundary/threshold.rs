903d58fedf7edba340977f58ae1fbf187e2f9a4b
Ken <lax.potcx1562@gmail.com>
2026-07-14 11:16:01 +0900

    threshold.rs拡張とテストOK

--- math/src/boundary/threshold.rs
+++ math/src/boundary/threshold.rs
@@ -6,3 +6,99 @@ pub struct Threshold<T> {
     order: Ordering,
     bound: Bound,
 }
+
+impl<T> Threshold<T> {
+    pub fn new(value: T, order: Ordering, bound: Bound) -> Self {
+        Self { value, order, bound }
+    }
+
+    pub fn value(&self) -> &T { &self.value }
+    pub fn order(&self) -> Ordering { self.order }
+    pub fn bound(&self) -> Bound { self.bound }
+}
+
+impl<T> Threshold<T>
+where
+    T: PartialOrd + PartialEq,
+{
+    pub fn contains(&self, value: &T) -> bool {
+        match (self.order, self.bound) {
+            (Ordering::Less, Bound::Inclusive) => value <= &self.value,
+            (Ordering::Less, Bound::Exclusive) => value < &self.value,
+
+            (Ordering::Greater, Bound::Inclusive) => value >= &self.value,
+            (Ordering::Greater, Bound::Exclusive) => value > &self.value,
+
+            (Ordering::Equal, _) => value == &self.value,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::boundary::Bound;
+
+    #[test]
+    fn threshold_less_exclusive() {
+        let t = Threshold::new(
+            30,
+            Ordering::Less,
+            Bound::Exclusive,
+        );
+
+        assert!(t.contains(&20));
+        assert!(t.contains(&29));
+        assert!(!t.contains(&30));
+        assert!(!t.contains(&40));
+    }
+
+    #[test]
+    fn threshold_less_inclusive() {
+        let t = Threshold::new(
+            30,
+            Ordering::Less,
+            Bound::Inclusive,
+        );
+
+        assert!(t.contains(&20));
+        assert!(t.contains(&30));
+        assert!(!t.contains(&31));
+    }
+
+    #[test]
+    fn threshold_greater_exclusive() {
+        let t = Threshold::new(
+            30,
+            Ordering::Greater,
+            Bound::Exclusive,
+        );
+
+        assert!(!t.contains(&30));
+        assert!(t.contains(&31));
+    }
+
+    #[test]
+    fn threshold_greater_inclusive() {
+        let t = Threshold::new(
+            30,
+            Ordering::Greater,
+            Bound::Inclusive,
+        );
+
+        assert!(t.contains(&30));
+        assert!(t.contains(&31));
+    }
+
+    #[test]
+    fn threshold_equal() {
+        let t = Threshold::new(
+            30,
+            Ordering::Equal,
+            Bound::Inclusive,
+        );
+
+        assert!(t.contains(&30));
+        assert!(!t.contains(&29));
+    }
+}
