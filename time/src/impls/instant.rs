4770607c15feca6ba0835f1d622f547e590d5164
Ken <lax.potcx1562@gmail.com>
2026-07-16 10:35:46 +0900

    time: 時間計算APIの未定義

--- time/src/impls/instant.rs
+++ time/src/impls/instant.rs
@@ -1,5 +1,8 @@
+use crate::duration::Duration;
 use crate::instant::Instant;
 use crate::traits::current::Current;
+use std::ops::{Add, Sub};
+use std::time::SystemTime;
 
 impl Current for Instant<std::time::Instant> {
     fn now() -> Self {
@@ -7,9 +10,63 @@ impl Current for Instant<std::time::Instant> {
     }
 }
 
-impl Current for Instant<std::time::SystemTime> {
+impl Current for Instant<SystemTime> {
     fn now() -> Self {
-        Self::from_trusted(std::time::SystemTime::now())
+        Self::from_trusted(SystemTime::now())
+    }
+}
+
+impl Add<Duration<std::time::Duration>>
+for Instant<std::time::Instant> {
+    type Output = Instant<std::time::Instant>;
+
+    fn add(self, rhs: Duration<std::time::Duration>) -> Self::Output {
+        todo!()
+    }
+}
+
+impl Add<Duration<std::time::Duration>>
+for Instant<SystemTime> {
+    type Output = Instant<SystemTime>;
+
+    fn add(self, rhs: Duration<std::time::Duration>) -> Self::Output {
+        todo!()
+    }
+}
+
+impl Sub<Duration<std::time::Duration>>
+for Instant<std::time::Instant> {
+    type Output = Instant<std::time::Instant>;
+
+    fn sub(self, rhs: Duration<std::time::Duration>) -> Self::Output {
+        todo!()
+    }
+}
+
+impl Sub<Duration<std::time::Duration>>
+for Instant<SystemTime> {
+    type Output = Instant<SystemTime>;
+
+    fn sub(self, rhs: Duration<std::time::Duration>) -> Self::Output {
+        todo!()
+    }
+}
+
+impl Sub<Instant<std::time::Instant>>
+for Instant<std::time::Instant> {
+    type Output = Duration<std::time::Duration>;
+
+    fn sub(self, rhs: Instant<std::time::Instant>) -> Self::Output {
+        todo!()
+    }
+}
+
+impl Sub<Instant<SystemTime>>
+for Instant<SystemTime>{
+    type Output = Duration<SystemTime>;
+
+    fn sub(self, rhs: Instant<SystemTime>) -> Self::Output {
+        todo!()
     }
 }
 
