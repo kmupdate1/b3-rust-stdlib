777cf5bd3f7be712e97f4bc30038d4d3840d0fe6
Ken <lax.potcx1562@gmail.com>
2026-07-16 11:27:36 +0900

    math: 演算操作系基盤追加

--- math/src/algebra/operation/mod.rs
+++ math/src/algebra/operation/mod.rs
@@ -8,8 +8,18 @@ mod closure;
 pub(crate) mod inverse;
 pub(crate) mod remainder;
 mod sqrt;
+pub mod add;
+pub mod sub;
+pub mod mul;
+pub mod div;
+pub mod neg;
 
 pub use closure::*;
 pub use inverse::*;
 pub use remainder::*;
 pub use sqrt::*;
+pub use add::*;
+pub use sub::*;
+pub use mul::*;
+pub use div::*;
+pub use neg::*;
