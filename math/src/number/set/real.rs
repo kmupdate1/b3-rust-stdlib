d08ce5be0c4f172ea16ac0d43366c76d16c53d46
Ken <lax.potcx1562@gmail.com>
2026-07-17 22:29:28 +0900

    math: real.rs 1. 計算まではおそらく正しい。 2. 残りは表示の問題。 3. 近似表示は未実装。

--- math/src/number/set/real.rs
+++ math/src/number/set/real.rs
@@ -1,37 +1,151 @@
-use crate::algebra::{Add, AdditiveInverse, Div, Mul, MultiplicativeInverse, Neg, Sub, Zero};
-use crate::number::RealError;
+use crate::algebra::{Add, AdditiveInverse, Div, Mul, MultiplicativeInverse, One, Sub, Zero};
+use crate::number::gcd::GreatestCommonDivisor;
+use crate::number::{Integer, Rational, RealError};
 use b3_core::validate::Validate;
-use std::fmt::{Display, Formatter};
+use std::fmt::{Display, Formatter, Pointer};
 
-#[derive(Debug, Clone, Copy, PartialEq)]
-pub struct Real<T> {
-    value: T,
+#[derive(Clone, PartialEq)]
+enum Constant<T: Integer> {
+    Rational(Rational<T>),
+    Pi,
+    E,
+    Phi,
 }
 
-impl<T> Real<T> {
-    #[deprecated(since = "0.2.0", note = "use `try_new` instead")]
-    pub fn new(value: T) -> Self {
-        Self { value }
-    }
+#[derive(Clone, Copy, PartialEq)]
+enum UnaryOperator {
+    Neg, Sqrt, Sin, Cos, Tan, Exp, Ln,
+}
 
-    pub fn value(&self) -> &T { &self.value }
-    pub fn into_inner(self) -> T { self.value }
+#[derive(Clone, Copy, PartialEq)]
+enum BinaryOperator {
+    Add, Sub, Mul, Div, Pow,
+}
+
+#[derive(Clone, PartialEq)]
+enum Expression<T: Integer> {
+    Constant(Constant<T>),
+    Unary {
+        op: UnaryOperator,
+        operand: Box<Expression<T>>,
+    },
+    Binary {
+        op: BinaryOperator,
+        lhs: Box<Expression<T>>,
+        rhs: Box<Expression<T>>,
+    },
+}
+
+#[derive(PartialEq)]
+pub struct Real<T>
+where
+    T: Integer,
+{
+    expr: Expression<T>,
 }
 
 impl<T> Real<T>
 where
-    T: Zero,
+    T: Integer + Clone + Div<Output = T> + GreatestCommonDivisor,
 {
-    pub fn try_new(value: T) -> Result<Self, RealError> {
-        let real = Self { value };
-        real.validate()?;
-        Ok(real)
+    pub fn pi() -> Self {
+        Self { expr: Expression::Constant(Constant::Pi) }
+    }
+    pub fn e() -> Self {
+        Self { expr: Expression::Constant(Constant::E) }
+    }
+    pub fn phi() -> Self {
+        Self { expr: Expression::Constant(Constant::Phi) }
+    }
+
+    pub fn neg(self) -> Self { self.inverse() }
+    pub fn sqrt(self) -> Self {
+        Self {
+            expr: Expression::Unary {
+                op: UnaryOperator::Sqrt,
+                operand: Box::new(self.expr),
+            }
+        }
+    }
+    pub fn sin(self) -> Self {
+        Self {
+            expr: Expression::Unary {
+                op: UnaryOperator::Sin,
+                operand: Box::new(self.expr),
+            }
+        }
+    }
+    pub fn cos(self) -> Self {
+        Self {
+            expr: Expression::Unary {
+                op: UnaryOperator::Cos,
+                operand: Box::new(self.expr),
+            }
+        }
+    }
+    pub fn tan(self) -> Self {
+        Self {
+            expr: Expression::Unary {
+                op: UnaryOperator::Tan,
+                operand: Box::new(self.expr),
+            }
+        }
+    }
+    pub fn exp(self) -> Self {
+        Self {
+            expr: Expression::Unary {
+                op: UnaryOperator::Exp,
+                operand: Box::new(self.expr),
+            }
+        }
+    }
+    pub fn ln(self) -> Self {
+        Self {
+            expr: Expression::Unary {
+                op: UnaryOperator::Ln,
+                operand: Box::new(self.expr),
+            }
+        }
+    }
+
+    pub fn pow(self, rhs: Self) -> Self {
+        Self {
+            expr: Expression::Binary {
+                op: BinaryOperator::Pow,
+                lhs: Box::new(self.expr),
+                rhs: Box::new(rhs.expr),
+            }
+        }
+    }
+}
+
+impl<T> One for Real<T>
+where
+    T: Integer + One + Clone + Div<Output = T> + GreatestCommonDivisor,
+{
+    fn one() -> Self {
+        Self {
+            expr: Expression::Constant(
+                Constant::Rational(Rational::one())
+            )
+        }
+    }
+}
+
+impl<T> From<Rational<T>> for Real<T>
+where
+    T: Integer,
+{
+    fn from(rational: Rational<T>) -> Self {
+        Self {
+            expr: Expression::Constant(Constant::Rational(rational)),
+        }
     }
 }
 
 impl<T> Validate for Real<T>
 where
-    T: Zero,
+    T: Integer + Zero,
 {
     type Error = RealError;
 
@@ -42,75 +156,176 @@ where
 
 impl<T> Add for Real<T>
 where
-    T: Add<Output = T>,
+    T: Integer + Add<Output = T>,
 {
     type Output = Self;
     fn add(self, rhs: Self) -> Self::Output {
-        Self { value: self.value + rhs.value }
+        Self {
+            expr: Expression::Binary {
+                op: BinaryOperator::Add,
+                lhs: Box::new(self.expr),
+                rhs: Box::new(rhs.expr),
+            }
+        }
     }
 }
 
 impl<T> Sub for Real<T>
 where
-    T: Sub<Output = T>,
+    T: Integer + Add<Output = T> + Clone + Div<Output = T> + GreatestCommonDivisor,
 {
     type Output = Self;
-    fn sub(self, rhs: Self) -> Self::Output { todo!() }
+    fn sub(self, rhs: Self) -> Self::Output { self + rhs.inverse() }
 }
 
 impl<T> Mul for Real<T>
 where
-    T: Mul<Output = T>,
+    T: Integer + Mul<Output = T>,
 {
     type Output = Self;
     fn mul(self, rhs: Self) -> Self::Output {
-        todo!()
+        Self {
+            expr: Expression::Binary {
+                op: BinaryOperator::Mul,
+                lhs: Box::new(self.expr),
+                rhs: Box::new(rhs.expr),
+            }
+        }
     }
 }
 
 impl<T> Div for Real<T>
 where
-    T: Div<Output = T>,
+    T: Integer + Mul<Output = T> + Clone + Div<Output = T> + GreatestCommonDivisor,
 {
-    type Output = Self;
-    fn div(self, rhs: Self) -> Self::Output {
-        todo!()
-    }
+    type Output = Result<Self, RealError>;
+    fn div(self, rhs: Self) -> Self::Output { Ok(self * rhs.try_inverse()?) }
 }
 
-impl<T> Neg for Real<T>
+impl<T> AdditiveInverse for Real<T>
 where
-    T: Neg<Output = T>,
+    T: Integer + Clone,
 {
-    type Output = Self;
-    fn neg(self) -> Self::Output {
-        todo!()
+    fn inverse(&self) -> Self {
+        Self {
+            expr: Expression::Unary {
+                op: UnaryOperator::Neg,
+                operand: Box::new(self.expr.clone()),
+            },
+        }
     }
+    fn invert(&mut self) { *self = self.inverse() }
 }
 
-impl<T> AdditiveInverse for Real<T> {
-    fn inverse(&self) -> Self { todo!() }
-    fn invert(&mut self) { todo!() }
-}
-
-impl<T> MultiplicativeInverse for Real<T> {
+impl<T> MultiplicativeInverse for Real<T>
+where
+    T: Integer + One + Clone + Div<Output = T> + GreatestCommonDivisor,
+{
     type Output = Self;
     type Error = RealError;
 
     fn try_inverse(&self) -> b3_core::error::Result<Self::Output, Self::Error> {
-        todo!()
+        Ok(Self {
+            expr: Expression::Binary {
+                op: BinaryOperator::Div,
+                lhs: Box::new(Expression::Constant(Constant::Rational(
+                    Rational::one()
+                ))),
+                rhs: Box::new(self.expr.clone()),
+            }
+        })
     }
     fn try_invert(&mut self) -> b3_core::error::Result<(), Self::Error> {
+        *self = self.try_inverse()?;
+        Ok(())
+    }
+}
+
+impl<T> Display for Constant<T>
+where
+    T: Integer + Display,
+{
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Constant::Rational(r) => write!(f, "{r}"),
+            Constant::Pi => write!(f, "π"),
+            Constant::E => write!(f, "e"),
+            Constant::Phi => write!(f, "φ")
+        }
+    }
+}
+
+impl Display for UnaryOperator {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            UnaryOperator::Neg => write!(f, "-"),
+            UnaryOperator::Sqrt => write!(f, "√"),
+            UnaryOperator::Sin => write!(f, "sin"),
+            UnaryOperator::Cos => write!(f, "cos"),
+            UnaryOperator::Tan => write!(f, "tan"),
+            UnaryOperator::Exp => write!(f, "exp"),
+            UnaryOperator::Ln => write!(f, "ln"),
+        }
+    }
+}
+
+impl Display for BinaryOperator {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            BinaryOperator::Add => write!(f, "+"),
+            BinaryOperator::Sub => write!(f, "-"),
+            BinaryOperator::Mul => write!(f, "*"),
+            BinaryOperator::Div => write!(f, "/"),
+            BinaryOperator::Pow => write!(f, "^"),
+        }
+    }
+}
+
+impl<T> Expression<T>
+where
+    T: Integer + Display,
+{
+    fn fmt_root(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Expression::Binary { op, lhs, rhs } => {
+                write!(f, "{lhs} {op} {rhs}")
+            }
+            _ => self.fmt(f)
+        }
+    }
+    fn fmt_nested(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         todo!()
+    }
+}
+
+impl<T> Display for Expression<T>
+where
+    T: Integer + Display,
+{
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Expression::Constant(c) => write!(f, "{c}"),
+            Expression::Unary { op, operand } => {
+                match op {
+                    UnaryOperator::Neg => write!(f, "{op}{operand}"),
+                    _ => write!(f, "{op}({operand})"),
+                }
+            }
+            Expression::Binary { op, lhs, rhs } => {
+                write!(f, "({lhs} {op} {rhs})")
+            }
+        }
     }
 }
 
 impl<T> Display for Real<T>
 where
-    T: Display,
+    T: Integer + Display,
 {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.value)
+        match self { &_ => {
+            write!(f, "{}", self.expr)
+        } }
     }
 }
 
@@ -119,41 +334,100 @@ mod tests {
     use super::*;
 
     #[test]
-    fn real_new() {
-        let real = Real::new(3.14);
+    fn real_from_rational() {
+        let rational = Rational::one();
 
-        assert_eq!(real.value(), &3.14);
+        let real: Real<i32> = rational.into();
+
+        assert!(real.validate().is_ok());
     }
 
     #[test]
-    fn real_into_inner() {
-        let real = Real::new(3.14);
+    fn real_pi() {
+        let pi = Real::<i32>::pi();
 
-        assert_eq!(real.into_inner(), 3.14);
+        assert_eq!(format!("{pi}"), "π");
     }
 
     #[test]
-    fn real_try_new_ok() {
-        let real = Real::try_new(3.14);
+    fn real_e() {
+        let e = Real::<i32>::e();
 
-        assert!(real.is_ok());
+        assert_eq!(format!("{e}"), "e");
     }
 
     #[test]
-    fn real_validate_ok() {
-        let real = Real::new(3.14);
+    fn real_phi() {
+        let phi = Real::<i32>::phi();
 
-        assert_eq!(real.validate(), Ok(()));
+        assert_eq!(format!("{phi}"), "φ");
     }
 
     #[test]
-    fn real_add_f32() {
-        let real_pi = Real::new(3.14f32);
-        let real_napier = Real::new(2.71f32);
+    fn real_one() {
+        let one = Real::<i32>::one();
 
-        let res = real_pi.add(real_napier);
+        assert_eq!(format!("{one}"), "1/1");
+    }
 
-        assert_ne!(res.value(), &5.85f32);
-        assert_ne!(res.value().to_bits(), 5.85f32.to_bits());
+    #[test]
+    fn neg() {
+        let x = Real::<i32>::pi();
+
+        // assert_eq!(format!("{}", -x), "-π");
+    }
+
+    #[test]
+    fn sqrt() {
+        let x = Real::<i32>::pi();
+
+        assert_eq!(format!("{}", x.sqrt()), "√π");
+    }
+
+    #[test]
+    fn sin() {
+        let x = Real::<i32>::pi();
+
+        assert_eq!(format!("{}", x.sin()), "sin(π)");
+    }
+
+    #[test]
+    fn add() {
+        let a = Real::<i32>::pi();
+        let b = Real::<i32>::e();
+
+        assert_eq!(format!("{}", a + b), "(π + e)");
+    }
+
+    #[test]
+    fn sub() {
+        let a = Real::<i32>::pi();
+        let b = Real::<i32>::e();
+
+        assert_eq!(format!("{}", a - b), "(π - e)");
+    }
+
+    #[test]
+    fn mul() {
+        let a = Real::<i32>::pi();
+        let b = Real::<i32>::e();
+
+        assert_eq!(format!("{}", a * b), "(π * e)");
+    }
+
+    #[test]
+    fn div() {
+        let a = Real::<i32>::pi();
+        let b = Real::<i32>::e();
+
+        assert_eq!(format!("{}", (a / b).unwrap()), "(π / e)");
+    }
+
+    #[test]
+    fn pow() {
+        let a = Real::<i32>::pi();
+        let b = Real::<i32>::from(Rational::one());
+
+        assert_eq!(format!("{}", a.pow(b)), "(π ^ 1/1)");
     }
 }
